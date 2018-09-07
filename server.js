41862fca8fdd5b94be346a42ee3d19565af18837
Lawrence Beck <sonny1388@msn.com>
2018-09-07 13:06:25 -0600

    getting ready to deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
+const path = require('path')
 const posts = require('./routes/api/posts')
 
 const app = express();
@@ -19,7 +19,18 @@ mongoose
 .catch(err=> console.log(err));
 
 //Use Routes
-app.use('/api/posts/', posts)
+app.use('/api/posts/', posts);
+
+//Serve static assests if in production
+if (process.env.NODE_ENV=== 'production'){
+//set static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+});
+
+}
 
 const port = process.env.PORT | 5000;
 
