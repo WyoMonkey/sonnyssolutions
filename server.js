fa9d8fe49a63c5e21225485cf07001d1b220d449
Lawrence Beck <sonny1388@msn.com>
2018-09-07 13:41:27 -0600

    server changes

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ mongoose
 app.use('/api/posts/', posts);
 
 //Serve static assests if in production
-if (process.env.NODE_ENV=== 'production'){
+if (process.env.NODE_ENV === 'production'){
+
 //set static folder
 app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
-res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 }
